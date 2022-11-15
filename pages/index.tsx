97a083ea9ae6b1f313da7f8f6b5068e791c16a46
zhoug <90252209+zhoug0x@users.noreply.github.com>
2022-11-15 10:39:27 -0400

    cleaned up UI flow for login, fixed window focus bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,81 +2,94 @@ import { useState, useEffect } from 'react'
 import { useEnsName } from 'wagmi'
 import { useWeb3 } from '../hooks/useWeb3'
 import SignInButton from '../components/SignInButton'
-
-interface HomePageState {
-  loggedInAddress?: string
-  error?: Error
-  loading?: boolean
-}
+import styles from '../styles/main.module.css'
 
 export default function HomePage() {
   const { connect, disconnect, isConnected, address, connectors } = useWeb3()
   const { data: ensName } = useEnsName({ address })
-  const [state, setState] = useState<HomePageState>({})
 
-  const fetchActiveUser = async () => {
+  const [loggedInAddress, setLoggedInAddress] = useState<string | undefined>()
+  const [error, setError] = useState<Error | undefined>()
+  const [isLoading, setIsLoading] = useState<boolean | undefined>()
+
+  const fetchLoggedInAddress = async () => {
     try {
+      setIsLoading(true)
       const result = await fetch('/api/me').then(res => res.json())
-      setState(x => ({ ...x, loggedInAddress: result.address }))
+      setLoggedInAddress(result.address)
+      setIsLoading(false)
     } catch (error) {
       console.error(error)
     }
   }
 
+  const handleLogout = async () => {
+    setIsLoading(true)
+    await fetch('/api/logout')
+    setLoggedInAddress(undefined)
+    setIsLoading(false)
+  }
+
   // fetch active user from backend if:
   useEffect(() => {
     // 1. page loads
-    fetchActiveUser()
-
-    // 2. window is focused (in case user logs out of another window)
-    window.addEventListener('focus', fetchActiveUser)
-    return () => window.removeEventListener('focus', fetchActiveUser)
+    fetchLoggedInAddress()
   }, [])
 
+  // if authenticated, add listener to refetch auth status on focus in case user logs out in a different window
   useEffect(() => {
-    console.log({ loggedInAddress: state.loggedInAddress })
-  }, [state.loggedInAddress])
+    if (loggedInAddress !== undefined) {
+      window.addEventListener('focus', fetchLoggedInAddress)
+      return () => window.removeEventListener('focus', fetchLoggedInAddress)
+    }
+  }, [loggedInAddress])
 
-  if (isConnected) {
+  // display UI based on wallet connection & authentication state
+  const renderUI = () => {
+    if (isLoading) {
+      return <div>LOADING...</div>
+    }
+
+    // is wallet connected?
+    if (isConnected) {
+      return (
+        <div>
+          <ul>
+            <li>connected: {address}</li>
+            <li>ens: {ensName ? ensName : '...'}</li>
+          </ul>
+          <button onClick={() => disconnect()}>disconnect</button>
+          <hr />
+          {/* is logged in? */}
+          {loggedInAddress ? (
+            <>
+              <div>logged in as {loggedInAddress}</div>
+              <button onClick={() => handleLogout()}>log out</button>
+            </>
+          ) : (
+            <SignInButton
+              onSuccess={({ loggedInAddress }) =>
+                setLoggedInAddress(loggedInAddress)
+              }
+              onError={({ error }) => setError(error)}
+            />
+          )}
+        </div>
+      )
+    }
+
+    // no wallet connection, is not logged in
     return (
       <div>
-        connected: {address}
-        <br />
-        ens: {ensName ? ensName : '...'}
-        {state.loggedInAddress ? (
-          <div>
-            <hr />
-            <div>logged in as {state.loggedInAddress}</div>
-            <button
-              onClick={async () => {
-                await fetch('/api/logout')
-                disconnect()
-                setState({})
-              }}
-            >
-              log out
-            </button>
-          </div>
-        ) : (
-          <SignInButton
-            onSuccess={({ loggedInAddress }) =>
-              setState(x => ({ ...x, loggedInAddress }))
-            }
-            onError={({ error }) => setState(x => ({ ...x, error }))}
-          />
-        )}
+        <p>connect wallet</p>
+        {connectors?.map(connector => (
+          <button key={connector.id} onClick={() => connect({ connector })}>
+            {connector.name}
+          </button>
+        ))}
       </div>
     )
-  }
+  } // end renderUI()
 
-  return (
-    <div>
-      <p>connect wallet</p>
-      {connectors?.map(connector => (
-        <button key={connector.id} onClick={() => connect({ connector })}>
-          {connector.name}
-        </button>
-      ))}
-    </div>
-  )
+  return <main className={styles.main}>{renderUI()}</main>
 }
